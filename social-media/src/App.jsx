a63f3d0558679a9c777648b1802adb413bcbfd31
Abubakar-Ciise <abubakarciise4@gmail.com>
2024-07-25 13:38:44 +0300

    first commit

--- social-media/src/App.jsx
+++ social-media/src/App.jsx
@@ -1,15 +1,35 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import AppContext from './components/AppContext/AppContext';
-import Pages from './Pages/Pages'; // Corrected path
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import LoginPage from "./pages/Login";
+import ProfilePage from "./pages/profile";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
+
   return (
-    <div className="App">
+    <div className="app">
       <BrowserRouter>
-        <AppContext>
-          <Pages />
-        </AppContext>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route
+              path="/home"
+              element={isAuth ? <HomePage /> : <Navigate to="/" />}
+            />
+            <Route
+              path="/profile/:userId"
+              element={isAuth ? <ProfilePage /> : <Navigate to="/" />}
+            />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
